8e1df54fd2d542905ebc61b18fa23a66fdad45b1
0104sheetal <sheetal.sharma@messold.com>
2024-02-05 18:14:40 +0530

    Updated Code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hello, World!');
+   res.send('Phuljhadi Custom Discount App!');
 });
 
 const port = process.env.PORT || 3000;
