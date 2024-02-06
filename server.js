d9aab7c0a341a0e77de3301ad903e38f9768b4fb
0104sheetal <158179620+0104sheetal@users.noreply.github.com>
2024-02-06 17:04:18 +0530

    Update Server File

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { run } = require('./extensions/order-discount-extension/src/run'); // Adjust the import path
 const app = express();
 
-app.use(bodyParser.json());
-
-app.post('/webhook/cart-update', (req, res) => {
-  const webhookData = req.body;
-
-  // Call the 'run' function to calculate discounts based on the webhook data
-  const discounts = run(webhookData);
-
-  // Implement code to apply the 'discounts' to the cart or perform other actions here
-
-  // Respond with a success status
-  res.status(200).send('Webhook received and processed successfully');
+app.get('/', (req, res) => {
+   res.send('Phuljhadi Custom Discount App!');
 });
 
 const port = process.env.PORT || 3000;
