308860c258d4eb98782ae96af4ecdcfb9a5b57a5
0104sheetal <158179620+0104sheetal@users.noreply.github.com>
2024-02-06 20:09:32 +0530

    Update run.js

--- extensions/order-discount-extension/src/run.js
+++ extensions/order-discount-extension/src/run.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { DiscountApplicationStrategy } from "../generated/api";
+const { DiscountApplicationStrategy } = require(“../generated/api”);
 // import {RunInput, FunctionRunResult, Target, ProductVariant } from "../generated/api";
 /**
  * @typedef {import("../generated/api").RunInput} RunInput
