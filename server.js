357ee7120fc89a0541170d78682aab602c8b6259
0104sheetal <sheetal.sharma@messold.com>
2024-02-13 12:04:19 +0530

    Updating Server File

--- server.js
+++ server.js
@@ -1,40 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
 
 const app = express();
 
-console.log('Inside Server')
-
-// Body parser middleware to handle JSON payloads
-app.use(bodyParser.json({ verify: (req, res, buf) => {
-  req.rawBody = buf;
-}}));
-
-// The route for Shopify webhooks
-app.post('/webhook', (req, res) => {
-  console.log('Inside Webhooks')
-  const hmac = req.get('X-Shopify-Hmac-Sha256');
-  const hash = crypto
-    .createHmac('sha256', 'SHOPIFY_SHARED_SECRET')
-    .update(req.rawBody, 'utf8', 'hex')
-    .digest('base64');
-    console.log('After Webhooks')
-
-  if (hash === hmac) {
-    console.log('Webhook verified');
-    // Handle the webhook data
-    console.log(req.body);
-
-    res.status(200).send('Webhook received');
-  } else {
-    console.log('Webhook verification failed');
-    res.status(403).send('Forbidden');
-  }
-});
-
 app.get('/', (req, res) => {
-  res.send('Phuljhadi Custom Discount App!!!');
+  res.send('Phuljhadi Custom Discount App!');
 });
 
 const port = process.env.PORT || 3000;
